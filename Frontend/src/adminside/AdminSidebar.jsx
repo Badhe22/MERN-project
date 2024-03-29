3e1cf90ecb6c3d9c5dbf46f9fef9adff76aeb3a2
Badhe22 <supriyabadhe86@gmail.com>
2024-03-29 11:57:45 +0530

    modify adminSidebar file

--- Frontend/src/adminside/AdminSidebar.jsx
+++ Frontend/src/adminside/AdminSidebar.jsx
@@ -4,19 +4,24 @@ import { Link } from 'react-router-dom';
 
 const AdminSidebar = () => {
   return (
-    <aside className="bg-violet-800 text-black p-4 ">
-      <div className="mb-6 bg-violet-800 p-1 ">
-        <h3 className="text-xl font-bold mb-2">Admin Panel</h3>
-        <ul>
-          <li>
-            <Link to="/admin/categories" className="block py-2 border-b border-gray-600">Categories</Link>
-          </li>
-          <li>
-            <Link to="/admin/products" className="block py-2 border-b border-gray-600">Products</Link>
-          </li>
-        </ul>
-      </div>
-    </aside>
+    <aside className="bg-yellow-400 text-black p-4 w-64 flex flex-col justify-between h-screen">
+  <div className="mb-6 bg-yellow-400 p-1">
+    <h3 className="text-xl font-bold mb-2">Admin Panel</h3>
+    <ul>
+      <li>
+        <Link to="/admin/categories" className="block py-4 mb-2 border-b border-gray-600">Categories</Link>
+      </li>
+      <li>
+        <Link to="/admin/products" className="block py-4 border-b border-gray-600">Products</Link>
+      </li>
+      <li>
+        <Link to="/admin/order" className="block py-4 border-b border-gray-600 mt-4">Orders</Link>
+      </li>
+    </ul>
+  </div>
+ 
+</aside>
+
   );
 }
 
